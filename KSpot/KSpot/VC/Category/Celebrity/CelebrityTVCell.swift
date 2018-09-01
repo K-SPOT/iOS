fd23a6740da60c6a9e3a8de5dac8466f58e76e00
강수진 <rkdthd1234@naver.com>
2018-09-02 06:34:11 +0900

    CelebrityVC 틀 만듦

---
+++ KSpot/KSpot/VC/Category/Celebrity/CelebrityTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  CelebrityTVCell.swift
+//  KSpot
+//
+//  Created by 강수진 on 2018. 9. 2..
+//  Copyright © 2018년 강수진. All rights reserved.
+//
+
+import UIKit
+
+class CelebrityTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
