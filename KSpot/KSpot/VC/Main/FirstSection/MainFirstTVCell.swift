8b7276f9da1f1f6098f896f96aa145d97fa9910c
강수진 <rkdthd1234@naver.com>
2018-09-18 02:36:27 +0900

    메인 첫번째 테이블뷰에 있는 컬렉션 뷰 정리

--- KSpot/KSpot/VC/Main/FirstSection/MainFirstTVCell.swift
+++ KSpot/KSpot/VC/Main/FirstSection/MainFirstTVCell.swift
@@ -11,34 +11,6 @@ import ImageSlideshow
 
 class MainFirstTVCell: UITableViewCell {
     
-   /* @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var pageControl: UIPageControl!
-    
-    var delegate : SelectSectionelegate?
-     var currentPages = 0
-     let sunglassArr = [#imageLiteral(resourceName: "aimg"),#imageLiteral(resourceName: "bimg"), #imageLiteral(resourceName: "cimg"), #imageLiteral(resourceName: "aimg"), #imageLiteral(resourceName: "bimg")]
-     override func awakeFromNib() {
-        super.awakeFromNib()
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
-        pageControl.numberOfPages = sunglassArr.count
-        pageControl.pageIndicatorTintColor = #colorLiteral(red: 0.8784313725, green: 0.8784313725, blue: 0.8784313725, alpha: 1)
-        pageControl.currentPageIndicatorTintColor = ColorChip.shared().mainColor
-
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        self.pageControl.currentPage = 0
-        self.collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .left, animated: false)
-    }*/
-  
     
     @IBOutlet var slideshow: ImageSlideshow!
     
@@ -75,60 +47,4 @@ class MainFirstTVCell: UITableViewCell {
     }
 }
 
-/*extension MainFirstTVCell : UICollectionViewDataSource, UICollectionViewDelegate {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sunglassArr.count
-    }
-    
-   
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        if let cell: MainFirstCVCell = collectionView.dequeueReusableCell(withReuseIdentifier: MainFirstCVCell.reuseIdentifier, for: indexPath) as? MainFirstCVCell
-        {
-           
-            
-            cell.myImgView.image = sunglassArr[indexPath.row]
-            return cell
-        }
-        return UICollectionViewCell()
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        delegate?.tap(section: .first, seledtedId: indexPath.row)
-    }
-}
 
-extension MainFirstTVCell: UICollectionViewDelegateFlowLayout {
-    //section내의
-    //-간격 위아래
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    //-간격 왼쪽오른쪽
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: self.frame.width, height: (364/375)*self.frame.width)
-        //return CGSize(width: 375, height: 375)
-    }
-}
-
-extension MainFirstTVCell : UIScrollViewDelegate{
-    
-    //ScrollView delegate method
-    func scrollViewDidScroll(_ scrollView: UIScrollView)
-    {
-        let pageWidth = scrollView.frame.width
-        self.currentPages = Int((scrollView.contentOffset.x + pageWidth / 2) / pageWidth)
-        self.pageControl.currentPage = self.currentPages
-    
-    }
-    
-}*/
