aa6aef27cb0faa74d2e6fccc83ec7d21d64b1331
강수진 <rkdthd1234@naver.com>
2018-09-18 19:14:54 +0900

    회원정보수정 완료 버튼 활성/비활성화

--- KSpot/KSpot/Util/Utility/Extensions.swift
+++ KSpot/KSpot/Util/Utility/Extensions.swift
@@ -83,6 +83,7 @@ extension UIViewController {
     func simpleAlert(title: String, message: String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "확인",style: .default)
+      
         alert.addAction(okAction)
         present(alert, animated: true)
     }
